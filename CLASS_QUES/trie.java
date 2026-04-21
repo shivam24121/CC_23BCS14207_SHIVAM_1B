1d72d1bdca8fb5dbe6dd7d4949e3527e7fa85075
shivam24121 <Shivam050905@gmail.com>
2026-04-21 14:44:32 +0530

    Create trie.java

---
+++ CLASS_QUES/trie.java
@@ -0,0 +1,70 @@
+class Trie {
+
+    class Node{
+        
+        Node[] child;
+        boolean eow;
+
+        Node(){
+            child=new Node[26];
+        }
+    }
+    public Node root;
+
+    public Trie() {
+        root=new Node();
+    }
+    
+    public void insert(String s) {
+
+        Node curr=root;
+
+        for(int i=0;i<s.length();i++){
+            int idx=s.charAt(i)-'a';
+            if(curr.child[idx]==null){
+                curr.child[idx]=new Node();
+            }
+            curr=curr.child[idx];
+        }
+        curr.eow=true;
+    }
+    
+    public boolean search(String word) {
+        
+        Node curr=root;
+
+        for(int i=0;i<word.length();i++){
+            int idx=word.charAt(i)-'a';
+
+            if(curr.child[idx]==null){
+                return false;
+            }
+            curr=curr.child[idx];
+        }
+        return curr.eow;
+    }
+    
+    public boolean startsWith(String prefix) {
+        
+        Node curr=root;
+
+        for(int i=0;i<prefix.length();i++){
+
+            int idx=prefix.charAt(i)-'a';
+
+            if(curr.child[idx]==null){
+                return false;
+            }
+            curr=curr.child[idx];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
